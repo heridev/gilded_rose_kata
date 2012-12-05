7e25f00ab7f59c31894997389bd84f971012d5c5
jose heriberto <heriberto.perez@crowdint.com>
2012-12-04 21:16:17 -0600

    Refactor the most important needed

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,50 +1,96 @@
+#def normal_item item
+  #unless 
+#end
+class ItemNames
+
+  def self.especial_items_name
+    {
+    aged: 'Aged Brie',
+    backstage: 'Backstage passes to a TAFKAL80ETC concert',
+    sulfuras: 'Sulfuras, Hand of Ragnaros'
+    }
+  end
+
+end
+
+class EvaluateRules
+  def initialize
+    @especial_items =  ItemNames.especial_items_name
+  end
+
+  #this method is used to evalute the all items and we return 
+  #true in case that the item name passed is diferent to the espcial item names
+  def especial_item? item_name
+    !@especial_items.find {|key, value| value == item_name}
+    #this is the same becasuse find and detect return the first value where is diferent to false
+    #ItemNames.especial_items.detect {|key, value| value == item_name}
+  end
+  def is_product_backstage? item_name
+    @especial_items[:backstage] == item_name
+  end
+
+  def is_product_sulfuras? item_name
+    @especial_items[:sulfuras] == item_name
+  end
+
+end
+
+
 def update_quality(items)
+  max_quality = 50
+  min_quality = 0
+  rules = EvaluateRules.new
+
+  items_name = ItemNames.especial_items_name
+
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
+    
+    if rules.especial_item? item.name
+      if item.quality > min_quality
+        item.quality -= 1
       end
     else
-      if item.quality < 50
+      if item.quality < max_quality
         item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if rules.is_product_backstage? item.name
           if item.sell_in < 11
-            if item.quality < 50
+            if item.quality < max_quality
               item.quality += 1
             end
           end
           if item.sell_in < 6
-            if item.quality < 50
+            if item.quality < max_quality
               item.quality += 1
             end
           end
         end
       end
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
+
+    unless rules.is_product_sulfuras? item.name
       item.sell_in -= 1
     end
+
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
+      if item.name != items_name[:aged]
+        unless rules.is_product_backstage? item.name
+          if item.quality > min_quality
+            unless rules.is_product_sulfuras? item.name
               item.quality -= 1
             end
           end
         else
-          item.quality = item.quality - item.quality
+          item.quality = 0
         end
       else
-        if item.quality < 50
+        if item.quality < max_quality
           item.quality += 1
         end
       end
     end
   end
 end
+
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
